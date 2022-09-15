3e3b9bb30d0cdc15e0abba8eb89018bc96345053
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-09-15 15:05:55 +0000

    finished

---
+++ ICS3U-Unit1-03.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 Emmanuel All rights reserved
+
+# Created by: Emmanuel
+# Created on: Sep 2022
+# ICS3U-Unit1-03.py File, Styling properly.
+
+
+def main():
+
+    print("Hello, World!")
+
+    print("n\Done.")
+
+
+if __name__ == "__main__":
+    main()
